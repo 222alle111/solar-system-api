896d812bf9e426e7ce26ceb2e691b608b100533a
Alejandra Guevara <alejandra.gue91@gmail.com>
2024-11-04 14:49:04 -0500

    add refactor

--- app/models/planet.py
+++ app/models/planet.py
@@ -14,3 +14,12 @@ class Planet(db.Model):
             description=self.description,
             size_dia_km=self.size_dia_km
         )
+    
+    @classmethod
+    def from_dict(cls, planet_data):
+        new_planet = cls(
+            name=planet_data["name"],
+            description=planet_data["description"],
+            size_dia_km=planet_data["size_dia_km"]
+        )
+        return new_planet
--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, abort, make_response, request, Response
 from app.models.planet import Planet
 from ..db import db
+from .routes_utilities import validate_model
 
 # created blueprint
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
@@ -8,11 +9,12 @@ planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 @planets_bp.post("")
 def create_planet():
     request_body = request.get_json()
-    name = request_body["name"]
-    description= request_body["description"]
-    size_dia_km = request_body["size_dia_km"]
+    try:
+        new_planet = Planet.from_dict(request_body)
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
 
-    new_planet = Planet(name=name, description=description, size_dia_km=size_dia_km)
     db.session.add(new_planet)
     db.session.commit()
 
@@ -47,13 +49,13 @@ def get_all_planets():
 
 @planets_bp.get("/<planet_id>")
 def get_single_planet(planet_id):
-    planet = validate_planet(planet_id)
+    planet = validate_model(Planet, planet_id)
     
     return planet.to_dict()
 
 @planets_bp.put("/<planet_id>")
 def update_planet(planet_id):
-    planet = validate_planet(planet_id)
+    planet = validate_model(Planet, planet_id)
     request_body = request.get_json()
 
     planet.name = request_body["name"]
@@ -66,24 +68,9 @@ def update_planet(planet_id):
 
 @planets_bp.delete("/<planet_id>")
 def delete_planet(planet_id):
-    planet = validate_planet(planet_id)
+    planet = validate_model(Planet, planet_id)
 
     db.session.delete(planet)
     db.session.commit()
     return Response(status=204, mimetype="application/json")
 
-
-def validate_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-
-    except ValueError:
-        abort(make_response({"message": f"planet {planet_id} is an invalid ID"}, 400))
-    
-    query = db.select(Planet).where(Planet.id == planet_id)
-    planet = db.session.scalar(query)
-    
-    if not planet:
-        abort(make_response({"message": f"planet {planet_id} not found"}, 404))
-
-    return planet 
